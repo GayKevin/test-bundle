f05d61bd97efe0f957739e461c9a1e3a7170975f
Thomas Chapuis <tchapuis@extellient.com>
2018-05-04 10:20:08 +0200

    First test

--- Tests/Sender/SenderTest.php
+++ Tests/Sender/SenderTest.php
@@ -4,12 +4,55 @@
 namespace Extellient\MailBundle\Tests\Sender;
 
 
+use Extellient\MailBundle\Entity\Mail;
+use Extellient\MailBundle\Provider\Mail\MailProviderInterface;
+use Extellient\MailBundle\Sender\MailSenderInterface;
+use Extellient\MailBundle\Sender\Sender;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class SenderTest
  */
 class SenderTest extends TestCase
 {
+    /**
+     * @var Sender
+     */
+    private $sender;
+    /**
+     * @var MailSenderInterface
+     */
+    private $mailSenderInterface;
+    /**
+     * @var MailProviderInterface
+     */
+    private $mailProviderInterface;
+    /**
+     * @var LoggerInterface
+     */
+    private $loggerInterface;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mailSenderInterface = $this->createMock(MailSenderInterface::class);
+        $this->mailProviderInterface = $this->createMock(MailProviderInterface::class);
+        $this->loggerInterface = $this->createMock(LoggerInterface::class);
+        $this->sender = new Sender($this->mailSenderInterface, $this->mailProviderInterface, $this->loggerInterface);
+    }
+
+    public function testGetMailLog()
+    {
+        $mail = new Mail('subject', 'body', ['recipient@test.com']);
+        $log = $this->sender->getMailLog($mail);
+        $this->assertInternalType('array',$log);
+        $this->assertCount(5, $log);
+        $this->assertArrayHasKey('recipients', $log);
+        $this->assertArrayHasKey('recipientsCopy', $log);
+        $this->assertArrayHasKey('recipentsHiddenCopy', $log);
+        $this->assertArrayHasKey('subject', $log);
+        $this->assertArrayHasKey('id', $log);
+    }
 
 }
